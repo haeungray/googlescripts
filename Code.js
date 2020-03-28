47c566c3057e02dbd0b555541097f770b6b1006f
haeungray <haeungray@gmail.com>
2020-03-28 21:56:16 +0900

    모듈 분리, 인삿말과 목적어 받는 함수 분기처리

--- Code.js
+++ Code.js
@@ -1,3 +1,4 @@
+/*
 function onAddToSpace(event) {
   console.info(event);
   var message = 'Thank you for adding me to ';
@@ -9,12 +10,11 @@ function onAddToSpace(event) {
   return { text: message };
 }
 
-
 function onRemoveFromSpace(event) {
   console.info(event);
   console.log('Bot removed from ', event.space.name);
 }
-
+*/
 var DEFAULT_IMAGE_URL = 'https://media.rocketpunch.com/cache/0b/98/0b98fd60b2e5d163260bf42590390eb3.png';
 var HEADER = {
   header: {
@@ -37,7 +37,7 @@ function createCardResponse(widgets) {
 
 var REASON = {
   SICK: 'Out sick',
-  OTHER: 'Out of office'
+  OTHER: '테스트로'
 };
 
 /**
@@ -50,54 +50,11 @@ function onMessage(event) {
   console.info(event);
   var reason = REASON.OTHER;
   var name = event.user.displayName;
-  var userMessage = event.message.text;
-
-  // If the user said that they were 'sick', adjust the image in the
-  // header sent in response.
-  if (userMessage.indexOf('sick') > -1) {
-    // Hospital material icon
-    HEADER.header.imageUrl = 'https://goo.gl/mnZ37b';
-    reason = REASON.SICK;
-  } else if (userMessage.indexOf('vacation') > -1) {
-    // Spa material icon
-    HEADER.header.imageUrl = 'https://goo.gl/EbgHuc';
+  var userMessage = event.message.argumentText;
+  userMessage = titleIndexing(userMessage)
+  if(Reason[userMessage] == 'INIT'){
+    var widgets = initMenu();
   }
-
-  var widgets = [{
-    textParagraph: {
-      text: '안녕하세요 스마트워크팀 입니다. <br/>' + name + '님 어떤 문의가 필요하신가요?'
-    }
-  },{
-    buttons: [{
-      textButton: {
-        text: 'nWiki 문의',
-        onClick: {
-          action: {
-            actionMethodName: 'turnOnAutoResponder',
-            parameters: [{
-              key: 'reason',
-              value: reason
-            }]
-          }
-        }
-      }
-    }]
-  },{
-    buttons: [{
-      textButton: {
-        text: 'Gsuite 문의 ',
-        onClick: {
-          action: {
-            actionMethodName: 'turnOnAutoResponder',
-            parameters: [{
-              key: 'reason',
-              value: reason
-            }]
-          }
-        }
-      }
-    }]
-  }];
   return createCardResponse(widgets);
 }
 
@@ -108,14 +65,30 @@ function onMessage(event) {
  * @see https://developers.google.com/hangouts/chat/reference/message-formats/events
  */
 
+function titleIndexing(msg){
+  // If the user said that they were 'sick', adjust the image in the
+  // header sent in response.
+  if (msg.indexOf('sick') > -1) {
+    // Hospital material icon
+    HEADER.header.imageUrl = 'https://goo.gl/mnZ37b';
+    reason = REASON.SICK;
+  } else if (msg.indexOf('vacation') > -1) {
+    // Spa material icon
+    HEADER.header.imageUrl = 'https://goo.gl/EbgHuc';
+  }
+  return reason
+}
+
 function onCardClick(event) {
   console.info(event);
   var message = '';
   var reason = event.action.parameters[0].value;
   if (event.action.actionMethodName == 'turnOnAutoResponder') {
+    console.info('event.action.parameters[0].value  '+ reason);
     turnOnAutoResponder(reason);
     message = 'Turned on vacation settings.';
   } else if (event.action.actionMethodName == 'blockOutCalendar') {
+    console.info('event.action.parameters[0].value  '+ reason);
     blockOutCalendar(reason);
     message = 'Blocked out your calendar for the day.';
   } else {
@@ -131,7 +104,7 @@ var ONE_DAY_MILLIS = 24 * 60 * 60 * 1000;
  * @param {string} reason the reason for vacation, either REASON.SICK or REASON.OTHER
  */
 
-function turnOnAutoResponder(reason) {
+function sendMailtoAdmin(reason) {
   var currentTime = (new Date()).getTime();
   Gmail.Users.Settings.updateVacation({
     enableAutoReply: true,
@@ -152,3 +125,45 @@ function turnOnAutoResponder(reason) {
 function blockOutCalendar(reason) {
   CalendarApp.createAllDayEvent(reason, new Date(), new Date(Date.now() + ONE_DAY_MILLIS));
 }
+
+
+function initMenu(){
+  var initWidget = [{
+    textParagraph: {
+      text: '안녕하세요 스마트워크팀 입니다. <br/>' + name + '님 저는 아직 성장중입니다!'
+    }
+  },{
+    buttons: [{
+      textButton: {
+        text: 'nWiki 문의',
+        onClick: {
+          action: {
+            actionMethodName: 'blockOutCalendar',
+            parameters: [{
+              key: 'reason',
+              value: reason
+            }]
+          }
+        }
+      }
+    }]
+  },{
+    buttons: [{
+      textButton: {
+        text: 'Gsuite 문의 ',
+        onClick: {
+          action: {
+            actionMethodName: 'blockOutCalendar',
+            parameters: [{
+              key: 'reason',
+              value: reason
+            }]
+          }
+        }
+      }
+    }]
+  }];
+  
+ return initWidget
+}
+
